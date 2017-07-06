67f4892dbfd97bf95c216359f89aaa6f5c9cb7c7
Andrii Kit <andrii.labyk@gmail.com>
2017-07-06 22:27:54 +0300

    1

---
+++ src/components/CommentBox.js
@@ -0,0 +1,41 @@
+import React, { Component} from 'react';
+
+export default class CommentBox extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comment: ''
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({
+            comment: ''
+        })
+    }
+    
+    render() {
+        return (
+            <form
+                className="comment-box"
+                onSubmit={this.handleSubmit.bind(this)}
+            >
+                <textarea
+                    onChange={this.handleChange.bind(this)}
+                    value={this.state.comment}
+                />
+                <button>Submit Comment</button>
+            </form>
+        );
+    }
+}
